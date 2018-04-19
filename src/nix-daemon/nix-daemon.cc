a7967f78bceef8ea8bbe96ccb7bf0f7b06c6a8c2
George Shammas <george@shamm.as>
2018-04-19 13:51:22 -0400

    Fix syslog calls to be secure

--- src/nix-daemon/nix-daemon.cc
+++ src/nix-daemon/nix-daemon.cc
@@ -307,7 +307,7 @@ static void performOp(TunnelLogger * logger, ref<LocalStore> store,
         ss << "op = queryValidPaths";
         for (auto path : paths)
           ss << "; path = " << path;
-        syslog(LOG_NOTICE, ss.str().c_str());
+        syslog(LOG_NOTICE, "%s", ss.str().c_str());
 
         logger->startWork();
         PathSet res = store->queryValidPaths(paths);
@@ -333,7 +333,7 @@ static void performOp(TunnelLogger * logger, ref<LocalStore> store,
         ss << "op = querySubstitutablePaths";
         for (auto path : paths)
           ss << "; path = " << path;
-        syslog(LOG_NOTICE, ss.str().c_str());
+        syslog(LOG_NOTICE, "%s", ss.str().c_str());
 
         logger->startWork();
         PathSet res = store->querySubstitutablePaths(paths);
@@ -464,7 +464,7 @@ static void performOp(TunnelLogger * logger, ref<LocalStore> store,
         ss << "sec = " << s;
         for (auto ref : refs)
           ss << "; ref = " << ref;
-        syslog(LOG_NOTICE, ss.str().c_str());
+        syslog(LOG_NOTICE, "%s", ss.str().c_str());
         logger->startWork();
         Path path = store->addTextToStore(suffix, s, refs, NoRepair);
         logger->stopWork();
@@ -512,7 +512,7 @@ static void performOp(TunnelLogger * logger, ref<LocalStore> store,
         ss << "mode = " << buildModeName(mode);
         for (auto path : drvs)
           ss << "; " << path;
-        syslog(LOG_NOTICE, ss.str().c_str());
+        syslog(LOG_NOTICE, "%s", ss.str().c_str());
 
         logger->startWork();
         store->buildPaths(drvs, mode);
@@ -601,7 +601,7 @@ static void performOp(TunnelLogger * logger, ref<LocalStore> store,
         ss << "maxFreed = " << options.maxFreed;
         for (auto path : options.pathsToDelete)
           ss << "; path = " << path;
-        syslog(LOG_NOTICE, ss.str().c_str());
+        syslog(LOG_NOTICE, "%s", ss.str().c_str());
 
         // obsolete fields
         readInt(from);
@@ -656,7 +656,7 @@ static void performOp(TunnelLogger * logger, ref<LocalStore> store,
                 overrides.emplace(name, value);
             }
         }
-        syslog(LOG_NOTICE, ss.str().c_str());
+        syslog(LOG_NOTICE, "%s", ss.str().c_str());
 
         logger->startWork();
 
@@ -727,7 +727,7 @@ static void performOp(TunnelLogger * logger, ref<LocalStore> store,
         ss << "op = querySubstitutablePathInfos";
         for (auto path : paths)
           ss << "; path = " << path;
-        syslog(LOG_NOTICE, ss.str().c_str());
+        syslog(LOG_NOTICE, "%s", ss.str().c_str());
 
         logger->startWork();
         SubstitutablePathInfos infos;
@@ -794,7 +794,7 @@ static void performOp(TunnelLogger * logger, ref<LocalStore> store,
         ss << "op = verifyStore; ";
         ss << "check = " << (checkContents ? "true" : "false") << "; ";
         ss << "repair = " << (repair ? "true" : "false");
-        syslog(LOG_NOTICE, ss.str().c_str());
+        syslog(LOG_NOTICE, "%s", ss.str().c_str());
 
         logger->startWork();
         if (repair && !trusted)
@@ -814,7 +814,7 @@ static void performOp(TunnelLogger * logger, ref<LocalStore> store,
         ss << "path = " << path;
         for (auto sig : sigs)
           ss << "; sig = " << sig;
-        syslog(LOG_NOTICE, ss.str().c_str());
+        syslog(LOG_NOTICE, "%s", ss.str().c_str());
 
         logger->startWork();
         if (!trusted)
@@ -865,7 +865,7 @@ static void performOp(TunnelLogger * logger, ref<LocalStore> store,
           ss << "; sig = " << sig;
         for (auto path : info.references)
           ss << "; path = " << path;
-        syslog(LOG_NOTICE, ss.str().c_str());
+        syslog(LOG_NOTICE, "%s", ss.str().c_str());
 
         TeeSink tee(from);
         parseDump(tee, tee.source);
@@ -884,7 +884,7 @@ static void performOp(TunnelLogger * logger, ref<LocalStore> store,
         ss << "op = queryMissing";
         for (auto target : targets)
           ss << "; path = " << target;
-        syslog(LOG_NOTICE, ss.str().c_str());
+        syslog(LOG_NOTICE, "%s", ss.str().c_str());
 
         logger->startWork();
         PathSet willBuild, willSubstitute, unknown;
